dfdef36b630efab6518288c98fec3e9b6d53d4e2
blurbdust <blurbdust@gmail.com>
2019-11-20 12:06:43 -0600

    adding parsing for custom shodan api search

--- genEye.py
+++ genEye.py
@@ -7,7 +7,7 @@ def format_masscan(ip, port, http, https, rdp, vnc):
 	if (https):
 		ret += "https://" + str(ip) + ":" + port + "/\n"
 	if (rdp):
-		ret += "rdp://" + str(ip) + ":" + port + "\n"
+		ret += "rdp://" + str(ip) + ":" + port + "/\n"
 	if (vnc):
 		ret += "vnc://" + str(ip) + ":" + port + "/\n"
 	return ret
@@ -23,6 +23,8 @@ def process_masscan(filename, outfile):
 
 	with open(filename, "r") as f:
 		for line in f:
+			if ("#" in line):
+				continue
 			http = False
 			https = False
 			rdp = False
@@ -30,8 +32,7 @@ def process_masscan(filename, outfile):
 			
 			lin = line.split(" ")
 
-			#print(lin)
-			ip = str(lin[5]).replace("\n", "")
+			ip = str(lin[5]).replace("\n", "").replace("\r", "").replace(" ", "")
 			port = str(str(lin[3]).split("/")[0])
 
 			if ("80" in port):
@@ -66,6 +67,62 @@ def process_masscan(filename, outfile):
 		else:
 			print(buf[:-1])
 
+def process_shodan(filename, outfile):
+	count = 0
+	buf = ""
+	cur = ""
+	output = None
+
+	if (outfile != None):
+		output = open(outfile, "w")			
+
+	with open(filename, "r") as f:
+		for line in f:
+			if ("#" in line):
+				continue
+			http = False
+			https = False
+			rdp = False
+			vnc = False
+			
+			ip 		= line.split(";")[0].split(":")[1].replace(" ", "")
+			ports 	= line.split(";")[1].split(":")[1].split(",")
+			
+			for port in ports:
+				port = port.replace(" ", "").replace("\n", "").replace("\r", "")
+				if ("80" in port):
+					#cur += "http://"
+					http = True
+				elif ("443" in port):
+					#cur += "https://"
+					https = True
+				elif ("3389" in port):
+					#cur += "rdp://"
+					rdp = True
+				elif ((int(port) < 5011) and (int(port) > 4999)):
+					#cur += "vnc://"
+					vnc = True
+				else:
+					cur = ""
+					continue
+
+				count += 1
+				cur = format_masscan(ip, port, http, https, rdp, vnc)
+				buf += cur
+				cur = ""
+
+			if ((count % 2000) == 0):
+				if (output != None):
+					output.write(buf)
+				buf = ""
+
+		if (output != None):
+			output.write(buf)
+		else:
+			print(buf[:-1])
+
+
+
 def ip_options(ip, http, https, rdp, vnc):
 	ret = ""
 	if (http != None):
@@ -73,34 +130,10 @@ def ip_options(ip, http, https, rdp, vnc):
 	if (https != None):
 		ret += "https://" + str(ip) + ":443/\n"
 	if (rdp != None):
-		ret += "rdp://" + str(ip) + ":3389\n"
+		ret += "rdp://" + str(ip) + ":3389/\n"
 	if (vnc != None):
-		ret += "vnc://" + str(ip) + ":5001\n"
+		ret += "vnc://" + str(ip) + ":5001/\n"
 	return ret
-
-def process_raw_file(infile, outfile, http, https, rdp, vnc):
-	cur = ""
-	buf = ""
-	count = 0
-	if (outfile != None):
-		with open(outfile, "w") as out:
-			#print(infile)
-			with open(infile, "r") as infil:
-				for line in infil:
-					line = line.replace("\n","").replace("\r","").replace("\t","")
-					count += 1
-
-					#print(line)
-					cur = ip_options(line, http, https, rdp, vnc)
-
-					buf += cur
-					cur = ""
-	
-					if ((count % 2000) == 0):
-						out.write(buf)
-						buf = ""
-			out.write(buf)
-
 
 def process_ips(outfile, http, https, rdp, vnc):
 	cur = ""
@@ -109,7 +142,7 @@ def process_ips(outfile, http, https, rdp, vnc):
 	if (outfile != None):
 		with open(outfile, "w") as out:
 			for arg in args.ip:
-				for ip in ipaddress.IPv4Network(arg, False):
+				for ip in ipaddress.IPv4Network(arg):
 					# saving output to file
 					# don't thrash io 
 					count += 1
@@ -125,7 +158,7 @@ def process_ips(outfile, http, https, rdp, vnc):
 			out.write(buf)
 	else:
 		for arg in args.ip:
-			for ip in ipaddress.IPv4Network(arg, False):
+			for ip in ipaddress.IPv4Network(arg):
 				print(ip_options(ip, http, https, rdp, vnc)[:-1])
 	#print("yee")
 
@@ -133,16 +166,16 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		formatter_class=argparse.RawDescriptionHelpFormatter,
 		description='Process a list or range of IPs to match EyeWitness\'s format', 
-		epilog=textwrap.dedent('''Examples:\npython3 genEye.py 192.168.1.0/24 192.168.2.0/24 --https --http\npython3 genEye.py --masscan tests/test.masscan.txt --outfile out.txt\npython3 genEye.py --raw ips.txt --outfile formatted.ips.txt --http --https''')
+		epilog=textwrap.dedent('''Examples:\npython3 genEye.py 192.168.1.0/24 192.168.2.0/24 --https --http\npython3 genEye.py --masscan tests/test.masscan.txt --outfile out.txt''')
 		)
-	parser.add_argument('--masscan', dest='load_file', help='Load list of IPs from a masscan ouput')
-	parser.add_argument('--raw', dest='load_file_raw', help='Load list of IPs from a file')
+	parser.add_argument('--masscan', dest='load_file', help='Load list of IPs from a file')
+	parser.add_argument('--shodan', dest='load_file_sho', help='Load list of IPs from a file (shodan)')
 	parser.add_argument('--outfile', dest='out_file', help='Output results to file')
 	parser.add_argument('ip', metavar='ip', type=str, nargs='*', help='IPs to parse')
-	parser.add_argument('--http', dest='http', const=sum, nargs='?', help='Used with ip or --raw, prepends http and postpends port 80')
-	parser.add_argument('--https', dest='https', const=sum, nargs='?', help='Used with ip or --raw, prepends https and postpends port 443')
-	parser.add_argument('--rdp', dest='rdp', const=sum, nargs='?', help='Used with ip or --raw, prepends rdp and postpends port 3389')
-	parser.add_argument('--vnc', dest='vnc', const=sum, nargs='?', help='Used with ip or --raw, prepends vnc and postpends port 5001')
+	parser.add_argument('--http', dest='http', const=sum, nargs='?', help='Used with ip, prepends http and postpends port 80')
+	parser.add_argument('--https', dest='https', const=sum, nargs='?', help='Used with ip, prepends https and postpends port 443')
+	parser.add_argument('--rdp', dest='rdp', const=sum, nargs='?', help='Used with ip, prepends rdp and postpends port 3389')
+	parser.add_argument('--vnc', dest='vnc', const=sum, nargs='?', help='Used with ip, prepends vnc and postpends port 5001')
 
 	args = parser.parse_args()
 
@@ -150,13 +183,9 @@ if __name__ == '__main__':
 
 	#print(args)
 	if (args.load_file != None):
-		#print("true")
-		#print("Parsing masscan file...")
 		process_masscan(args.load_file, args.out_file)
-	elif (args.load_file_raw != None):
-		if ((args.http == None) and (args.https == None) and (args.rdp == None) and (args.vnc == None)):
-			print("Please add arguments for http, https, rdp, or vnc.")
-		process_raw_file(args.load_file_raw, args.out_file, args.http, args.https, args.rdp, args.vnc)
+	elif (args.load_file_sho != None):
+		process_shodan(args.load_file_sho, args.out_file)
 	elif (args.ip != []):
 		#print("Expanding IPs...")
 		process_ips(args.out_file, args.http, args.https, args.rdp, args.vnc)
