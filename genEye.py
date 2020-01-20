4f4ef9e516a7c5cb3ac8d01307670f1db853bb9b
blurbdust <blurbdust@gmail.com>
2020-01-20 15:04:07 -0600

    added nmap -oG parsing

--- genEye.py
+++ genEye.py
@@ -1,5 +1,79 @@
 import sys, argparse, ipaddress, textwrap
 
+def format_nmap(ip, port, http, https, rdp, vnc):
+	ret = ""
+	if (http):
+		ret += "http://" + str(ip) + ":" + port + "/\n"
+	if (https):
+		ret += "https://" + str(ip) + ":" + port + "/\n"
+	if (rdp):
+		ret += "rdp://" + str(ip) + ":" + port + "\n"
+	if (vnc):
+		ret += "vnc://" + str(ip) + ":" + port + "\n"
+	return ret
+
+def process_nmap(filename, outfile):
+	count = 0
+	buf = ""
+	cur = ""
+	output = None
+
+	if (outfile != None):
+		output = open(outfile, "w")			
+
+	with open(filename, "r") as f:
+		for line in f:
+			if ("#" in line):
+				continue
+			if ("Ports:" not in line):
+				continue
+			http = False
+			https = False
+			rdp = False
+			vnc = False
+			
+			lin = line.split(" ")
+
+			ip = str(lin[1]).replace("\n", "").replace("\r", "").replace(" ", "")
+			#consume until we hit "Ports"
+			lin = line[line.index("Ports:") + len("Ports: "):]
+			ports = str(str(lin).split("  ")[0]).split(" ")
+			for port in ports:
+				port = port.split("/")[0]
+
+				if ("80" in port):
+					#cur += "http://"
+					http = True
+				elif ("43" in port):
+					#cur += "https://"
+					https = True
+				elif ("3389" in port):
+					#cur += "rdp://"
+					rdp = True
+				elif ((int(port) < 5011) and (int(port) > 4999)):
+					#cur += "vnc://"
+					vnc = True
+				else:
+					cur = ""
+					continue
+
+				count += 1
+
+				cur = format_nmap(ip, port, http, https, rdp, vnc)
+				buf += cur
+				cur = ""
+
+				if ((count % 2000) == 0):
+					if (output != None):
+						output.write(buf)
+					buf = ""
+
+		if (output != None):
+			output.write(buf)
+		else:
+			print(buf[:-1])
+
+
 def format_masscan(ip, port, http, https, rdp, vnc):
 	ret = ""
 	if (http):
@@ -131,7 +205,7 @@ def ip_options(ip, http, https, rdp, vnc, none_):
 	if (https != None):
 		ret += "https://" + str(ip) + ":443/\n"
 	if (rdp != None):
-		ret += "rdp://" + str(ip) + ":3389/\n"
+		ret += "rdp://" + str(ip) + ":3389\n"
 	if (vnc != None):
 		ret += "vnc://" + str(ip) + ":5001\n"
 	if (none_ != None):
@@ -193,10 +267,12 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		formatter_class=argparse.RawDescriptionHelpFormatter,
 		description='Process a list or range of IPs to match EyeWitness\'s format', 
-		epilog=textwrap.dedent('''Examples:\npython3 genEye.py 192.168.1.0/24 192.168.2.0/24 --https --http\npython3 genEye.py --masscan tests/test.masscan.txt --outfile out.txt''')
+		epilog=textwrap.dedent('''Examples:\npython3 genEye.py 192.168.1.0/24 192.168.2.0/24 --https --http\npython3 genEye.py --masscan tests/test.masscan.txt --outfile out.txt\npython3 genEye.py --namp tests/test.nmap.txt --outfile out.txt''')
 		)
 	parser.add_argument('--masscan', dest='load_file', help='Load list of IPs from a file')
+	parser.add_argument('--nmap', dest='load_file_nmap', help='Load list of IPs from a file (nmap')
 	parser.add_argument('--shodan', dest='load_file_sho', help='Load list of IPs from a file (shodan)')
+	parser.add_argument('--raw', dest='load_file_raw', help='Load list of IPs from a file (raw)')
 	parser.add_argument('--outfile', dest='out_file', help='Output results to file')
 	parser.add_argument('ip', metavar='ip', type=str, nargs='*', help='IPs to parse')
 	parser.add_argument('--http', dest='http', const=sum, nargs='?', help='Used with ip or --raw, prepends http and postpends port 80')
@@ -218,6 +294,8 @@ if __name__ == '__main__':
 		process_raw_file(args.load_file_raw, args.out_file, args.http, args.https, args.rdp, args.vnc, args.none_)
 	elif (args.load_file_sho != None):
 		process_shodan(args.load_file_sho, args.out_file)
+	elif (args.load_file_nmap != None):
+		process_nmap(args.load_file_nmap, args.out_file)
 	elif (args.ip != []):
 		#print("Expanding IPs...")
 		process_ips(args.out_file, args.http, args.https, args.rdp, args.vnc, args.none_)
